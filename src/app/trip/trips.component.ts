db8e1fb0527ebaab47b9fa0bbb79d7e392bd4b9e
karloscode <carlosjaviergonzalezolmo@gmail.com>
2016-12-17 13:01:42 +0100

    Alpha 0.0.1

--- src/app/trip/trips.component.ts
+++ src/app/trip/trips.component.ts
@@ -3,8 +3,6 @@ import { Router } from '@angular/router';
 import { Trip } from './trip';
 import { TripService } from './trip.service';
 import { AngularFire, FirebaseApp } from 'angularfire2';
-//import { UserService } from '../users/user.service';
-
 import { TripsFormComponent } from './trips-form.component';
 import { FirebaseListObservable } from 'angularfire2';
 
@@ -18,44 +16,40 @@ export class TripsComponent implements OnInit {
   trips: FirebaseListObservable<any[]>;
   path: string ='/trips';
   public userData: any;
-  public auth: any;
+  public authF: any;
   
 
   constructor(@Inject(FirebaseApp) firebaseApp: any , private tripService: TripService ,
     private af: AngularFire ) {
-    this.auth = firebaseApp.auth();
-
+    this.authF = firebaseApp.auth();
    }
 
  	ngOnInit(): void {
     this.trips = this.tripService.getTrips();
-    console.log(this.auth);
     this.af.auth.subscribe(auth => {
-          console.log("oninit trips"+auth.uid);
           this.userData = auth;
-          console.log("oninit trips userdata"+userData.value);
         });
   }
 
-
   onSelect(trip: Trip): void {
-    	this.selectedTrip = trip;
+    this.selectedTrip = trip;
   }
 
   new_Trip(newTrip){
 
-       if(this.userData.valid) {
+      /* if(this.userData.valid) {
       console.log(this.userData.value);
-      this.auth.currentUser.updateProfile(this.userData.value)
+      this.authF.currentUser.updateProfile(this.userData.value)
         .then((success) => {
           console.log('Success', success);
         })
         .catch((error) => {
           console.log(error);
         })
-    }
-
-     this.tripService.newTrip(newTrip);
+    }*/
+    newTrip.userName = this.userData.auth.displayName;
+    newTrip.uid = this.userData.auth.uid;
+    this.tripService.newTrip(newTrip);
   }
 
 }
