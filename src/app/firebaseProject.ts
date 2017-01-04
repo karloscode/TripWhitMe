113d2ff3fc8fee2e3b9329243692014da240b438
karloscode <carlosjaviergonzalezolmo@gmail.com>
2017-01-04 02:21:09 +0100

    gitpages

---
+++ src/app/firebaseProject.ts
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDmQkO9gm5nJfrCsbr3I3esjxk-Fg6La2o",
+    authDomain: "test-7e1d0.firebaseapp.com",
+    databaseURL: "https://test-7e1d0.firebaseio.com",
+    storageBucket: "test-7e1d0.appspot.com",
+    messagingSenderId: "981841855176"
+};
