6dfb1253d11dfa9147e8234f9daac9ff912a3290
karloscode <carlosjaviergonzalezolmo@gmail.com>
2017-02-20 11:40:11 +0100

    CSS file opt

--- src/app/trip/trips-form.component.ts
+++ src/app/trip/trips-form.component.ts
@@ -15,60 +15,60 @@ export class TripsFormComponent {
 
 
 	 id = "chartdiv";
-   // svg path for target icon
-   targetSVG = "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z";
-  // svg path for plane icon
-  planeSVG = "m2,106h28l24,30h72l-44,-133h35l80,132h98c21,0 21,34 0,34l-98,0 -80,134h-35l43,-133h-71l-24,30h-28l15,-47";
+ 	 // svg path for target icon
+  	 targetSVG = "M9,0C4.029,0,0,4.029,0,9s4.029,9,9,9s9-4.029,9-9S13.971,0,9,0z M9,15.93 c-3.83,0-6.93-3.1-6.93-6.93S5.17,2.07,9,2.07s6.93,3.1,6.93,6.93S12.83,15.93,9,15.93 M12.5,9c0,1.933-1.567,3.5-3.5,3.5S5.5,10.933,5.5,9S7.067,5.5,9,5.5 S12.5,7.067,12.5,9z";
+ 	 // svg path for plane icon
+ 	 planeSVG = "m2,106h28l24,30h72l-44,-133h35l80,132h98c21,0 21,34 0,34l-98,0 -80,134h-35l43,-133h-71l-24,30h-28l15,-47";
 
 
-   chart = {
-    "type": "map",
-    "theme": "light",
-    "dataProvider": {
-    "map": "worldLow",
-    "zoomLevel": 1.1,
-    "zoomLongitude": 0,
-    "zoomLatitude": 0,
+   	chart = {
+		"type": "map",
+		"theme": "light",
+		"dataProvider": {
+		"map": "worldLow",
+		"zoomLevel": 1.1,
+		"zoomLongitude": 0,
+		"zoomLatitude": 0,
 
-    "lines": [ ],
-    "images": [ ]
-  },
+		"lines": [ ],
+		"images": [ ]
+		},
 
-  "areasSettings": {
-    "unlistedAreasColor": "#286090",
-    "unlistedAreasAlpha": 1
-  },
+		"areasSettings": {
+			"unlistedAreasColor": "#286090",
+			"unlistedAreasAlpha": 1
+		},
 
-  "imagesSettings": {
-    "color": "#CC0000",
-    "rollOverColor": "#CC0000",
-    "selectedColor": "#000000"
-  },
+		"imagesSettings": {
+			"color": "#CC0000",
+			"rollOverColor": "#CC0000",
+			"selectedColor": "#000000"
+		},
 
-  "linesSettings": {
-    "arc": -0.85, // this makes lines curved. Use value from -1 to 1
-    "arrow": "middle",
-    "color": "#CC0000",
-    "alpha": 1,
-    "arrowAlpha": 1,
-    "arrowSize": 4
-  },
-  "zoomControl": {
-    "gridHeight": 100,
-    "draggerAlpha": 1,
-    "gridAlpha": 0.2
-  },
+		"linesSettings": {
+			"arc": -0.85, // this makes lines curved. Use value from -1 to 1
+			"arrow": "middle",
+			"color": "#CC0000",
+			"alpha": 1,
+			"arrowAlpha": 1,
+			"arrowSize": 4
+		},
+		"zoomControl": {
+			"gridHeight": 100,
+			"draggerAlpha": 1,
+			"gridAlpha": 0.2
+		},
 
-  "backgroundZoomsToTop": true,
-  "linesAboveImages": true, 
-  "pauseDuration": 0.2,
-  "animationDuration": 2.5,
-  "adjustAnimationSpeed": true,
-  
-  "export": {
-    "enabled": false
-  }
-}
+		"backgroundZoomsToTop": true,
+		"linesAboveImages": true, 
+		"pauseDuration": 0.2,
+		"animationDuration": 2.5,
+		"adjustAnimationSpeed": true,
+		
+		"export": {
+			"enabled": false
+		}
+	}
 	submitted = false;
 	onSubmit(){
 		this.submitted = true;
@@ -144,43 +144,43 @@ export class TripsFormComponent {
 			"zoomLatitude": 0,
 
 			"lines": [ {
-			"id": "line1",
-			"arc": -0.85,
-			"color": "#CC0000",
-     		"alpha": 0.3,
-			"latitudes": [ this.placeFrom.lat, this.placeTo.lat ],
-			"longitudes": [ this.placeFrom.lng, this.placeTo.lng ]
+				"id": "line1",
+				"arc": -0.85,
+				"color": "#CC0000",
+				"alpha": 0.3,
+				"latitudes": [ this.placeFrom.lat, this.placeTo.lat ],
+				"longitudes": [ this.placeFrom.lng, this.placeTo.lng ]
 			} ],
 			"images": [ {
-			"svgPath": this.targetSVG,
-			"title": this.placeFrom.name,
-			"latitude": this.placeFrom.lat,
-			"longitude": this.placeFrom.lng,
-			"scale": 1.5
-			}, {
-			"svgPath": this.targetSVG,
-			"title": this.placeTo.name,
-			"latitude": this.placeTo.lat,
-			"longitude": this.placeTo.lng,
-			"scale": 1.5
-			}, {
-			"svgPath": this.planeSVG,
-			"positionOnLine": 0,
-			"color": "#CC0000",
-			"animateAlongLine": true,
-			"lineId": "line1",
-			"flipDirection": true,
-			"loop": true,
-			"scale": 0.05,
-			"positionScale": 1.8
-			}, {	   
-			"backgroundZoomsToTop": true,
-			"linesAboveImages": true, 
-			"pauseDuration": 0.2,
-			"animationDuration": 2.5,
-			"adjustAnimationSpeed": true,
-			}
-			]
+				"svgPath": this.targetSVG,
+				"title": this.placeFrom.name,
+				"latitude": this.placeFrom.lat,
+				"longitude": this.placeFrom.lng,
+				"scale": 1.5
+				}, {
+				"svgPath": this.targetSVG,
+				"title": this.placeTo.name,
+				"latitude": this.placeTo.lat,
+				"longitude": this.placeTo.lng,
+				"scale": 1.5
+				}, {
+				"svgPath": this.planeSVG,
+				"positionOnLine": 0,
+				"color": "#CC0000",
+				"animateAlongLine": true,
+				"lineId": "line1",
+				"flipDirection": true,
+				"loop": true,
+				"scale": 0.05,
+				"positionScale": 1.8
+				}, {	   
+				"backgroundZoomsToTop": true,
+				"linesAboveImages": true, 
+				"pauseDuration": 0.2,
+				"animationDuration": 2.5,
+				"adjustAnimationSpeed": true,
+				}
+				]
 		};
         // console.log("lat:"+this.placeTo.lat, "lng:"+this.placeTo.lng, "adress:"+this.placeTo.name);
          }
